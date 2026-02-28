545d5c03e4f08da7e45258cbc9f16c19d79c05a8
1gmo <devmaze@proton.me>
2026-02-28 17:41:25 +0200

    fix: use ts-node loader for knex in CI pwd correct

--- knexfile.ts
+++ knexfile.ts
@@ -1,17 +1,15 @@
-// knexfile.js
+// knexfile.ts
 import 'dotenv/config';
 
 export default {
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 5432,
-  },
+  connection: process.env.DATABASE_URL,
   migrations: {
-  directory: './migrations',
-  extension: 'ts'
-}
+    directory: './migrations',
+    extension: 'ts'
+  },
+  seeds: {
+    directory: './seeds',
+    extension: 'ts'
+  }
 };
